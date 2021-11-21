e16657ea920ba881d0d2c65533b0504f1ab4693a
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-20 22:03:12 -0600

    page element design and scss

--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -1,13 +1,24 @@
 import './topbar.scss'
+import {Person, Mail} from '@material-ui/icons'
 
 export default function Topbar() {
     return (
         <div className='topbar'>
            <div className='wrapper'>
            <div className='left'>
-           <a href='#intro'>Hello its me</a>
+           <a href='#intro'className='logo'>neonilla.</a>
+            <div className='itemContainer'>
+            <Person className='icon'/>
+            <span> 858 405 7032</span>
+            </div>
+            <div className='itemContainer'>
+            <Mail className='icon' />
+            <span> nellia.f.charlton@gmail.com</span>
+            </div>
+
             </div>  
-           <div className='right'> </div>  
+            <h1></h1>
+           <div className='right'></div>  
            </div> 
         </div>
     )
