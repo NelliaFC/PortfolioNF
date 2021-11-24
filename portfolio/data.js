2dbbafa0e92618c3d2d8b61243a538a97128a60f
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-24 09:56:42 -0600

    use Effect

---
+++ portfolio/data.js
@@ -0,0 +1,33 @@
+
+
+export const mobilePortfolio = [
+    {
+        id: 1,
+        title: "mobile Social Media App",
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Ff8%2Fa9%2F00%2Ff8a900d3d0686a1b68c903260f96d755.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F380624605986098221%2F&tbnid=eVC0Iv61f7xNeM&vet=12ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygJegUIARDCAQ..i&docid=BRNe3grC1seGyM&w=1366&h=768&q=google%20images&hl=en&ved=2ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygJegUIARDCAQ",
+    },
+
+    {
+        id: 2,
+        title: " Media App",
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pandotrip.com%2Fwp-content%2Fuploads%2F2015%2F08%2FTop-10-Google-Great-Sandy-Photo-by-Google-Earth.jpg&imgrefurl=https%3A%2F%2Fwww.pandotrip.com%2Ftop-10-google-earth-images-from-the-earth-20419%2F&tbnid=6ld1tptTFPIRQM&vet=12ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygIegUIARDAAQ..i&docid=KdIx-EkCKGd4TM&w=1000&h=667&q=google%20images&hl=en&ved=2ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygIegUIARDAAQ",
+    },
+
+    {
+        id:3,
+        title: " App",
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdata.whicdn.com%2Fimages%2F241700911%2Foriginal.jpg&imgrefurl=https%3A%2F%2Fweheartit.com%2Fentry%2F241700911&tbnid=kP8-YxwuhXc2xM&vet=12ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygMegUIARDIAQ..i&docid=CXFmcEBanGOyOM&w=1280&h=960&q=google%20images&hl=en&ved=2ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygMegUIARDIAQ",
+    },
+
+    {
+        id: 4,
+        title: "mobile ",
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdata.whicdn.com%2Fimages%2F241700911%2Foriginal.jpg&imgrefurl=https%3A%2F%2Fweheartit.com%2Fentry%2F241700911&tbnid=kP8-YxwuhXc2xM&vet=12ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygMegUIARDIAQ..i&docid=CXFmcEBanGOyOM&w=1280&h=960&q=google%20images&hl=en&ved=2ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygMegUIARDIAQ",
+    },
+
+    {
+        id: 5,
+        title: "mobile  App",
+        img: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.pandotrip.com%2Fwp-content%2Fuploads%2F2015%2F08%2FTop-10-Google-Great-Sandy-Photo-by-Google-Earth.jpg&imgrefurl=https%3A%2F%2Fwww.pandotrip.com%2Ftop-10-google-earth-images-from-the-earth-20419%2F&tbnid=6ld1tptTFPIRQM&vet=12ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygIegUIARDAAQ..i&docid=KdIx-EkCKGd4TM&w=1000&h=667&q=google%20images&hl=en&ved=2ahUKEwjBlZOFqbH0AhVP_6wKHZhFCuMQMygIegUIARDAAQ",
+    },
+]
--- portfolio/src/components/portfolio/Portfolio.jsx
+++ portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,17 @@
 import './portfolio.scss';
 import PortfolioList from "../portfolioList/PortfolioList"
 import { useEffect, useState } from "react";
+import { 
+  featuredPortfolio,
+  webPortfolio,
+  mobilePortfolio,
+  designPortfolio,
+  contenPortfolio,
+} from '../../data.js'
 
 export default function Portfolio() {
-  const [selected, setSelected] = useState("featured")
+  const [selected, setSelected] = useState("featured");
+  const[data, setData] = useState([])
 
     const list =[
         {
@@ -27,6 +35,29 @@ export default function Portfolio() {
             title: "Content",
           },
         ];
+
+        useEffect(() =>{
+          switch(selected){
+            case "featured":
+              setData(featuredPortfolio);
+              break;
+              case "web":
+              setData(webPortfolio);
+              break;
+              case "mobile":
+              setData(mobilePortfolio);
+              break;
+              case "design":
+              setData(designPortfolio);
+              break;
+              case "content":
+              setData(contentPortfolio);
+              break;
+              default: 
+              setData(featuredPortfolio);
+          }
+
+        }, [selected])
 
     return (
         <div className='portfolio' id='portfolio'>
