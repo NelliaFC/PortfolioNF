56cf37b17e49ee23e0ac1d93aed617428887fb9d
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-27 16:57:57 -0600

    works page css troubleshooting;

--- portfolio/src/components/works/Works.jsx
+++ portfolio/src/components/works/Works.jsx
@@ -1,16 +1,25 @@
 import './works.scss'
 
+
 export default function Works() {
     return <div className="works" id="works">
         <div className="slider">
             <div className="container">
                 <div className="item">
-                    <div className="left"></div>
+                    <div className="left">
+                        <div className="leftContainer">
+                        <img src="assets/mobile.png" alt=""/>
+                    </div>
+                    <h2>Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adip
+                    Lorem Ipsum
+                    </p>
+                    </div>
+                    </div>
                     <div className="right"></div>
                 </div>
             </div>
         </div>
-    </div>
-       
     
+       
 }
