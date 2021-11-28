d4cfbace42810b8689ca6c747e953a46734696db
NeoNeonilla <nelliafleurova@gmail.com>
2021-11-28 11:34:53 -0600

    contact page beginnign

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -3,7 +3,16 @@ import './contact.scss'
 export default function Contact() {
     return (
         <div className='contact' id='contact'>
-           
+        <div className='left'>
+            <img src='assets/shake.svg' alt='shake' />
+        </div>
+        <div className='right'></div>
+        <h2>Contact.</h2>
+           <form>
+               <input type="text"  placeholder='Email'/>
+               <textarea placeholder="Message"></textarea>
+               <button type='submit'>Send</button>
+           </form>  
         </div>
     )
 }
