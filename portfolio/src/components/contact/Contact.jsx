87caef47c2e3e7f2b9e90033edd6a608f3d32f21
NeoNeonilla <nelliafleurova@gmail.com>
2021-12-02 21:15:36 -0600

    final commit

--- portfolio/src/components/contact/Contact.jsx
+++ portfolio/src/components/contact/Contact.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
     return (
       <div className="contact" id="contact">
         <div className="left">
-          <img style={{marginLeft:'80px'}}src="assets/contactback.png" alt="" />
+          <img style={{marginLeft:'80px'}}src="assets/livefully.png" alt="" />
         </div>
         <div className="right">
           <h2>Contact.</h2> 
